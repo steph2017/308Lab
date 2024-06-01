e1c0510ee410a178736b61bd2c7bf89d71122686
StephOg <stephanieogwo@Stephanies-MacBook-Pro.local>
2024-06-01 09:33:23 -0400

    created variables for each parameter, declared function

---
+++ 308SBA.js
@@ -0,0 +1,33 @@
+// Declare 4 object data types
+
+const CInfo = {
+    id: 0,
+    name: "name"
+
+};
+
+const AGroup = {
+    id: 0,
+    name: "name",
+    course_id: "course",
+    group_weight: 0,
+    assignments: []
+};
+
+const AInfo = {
+    id: 0,
+    name: "name",
+    due_at: "yyyymmdd",
+    points_possible: 0
+};
+
+const LSubmission = [];
+// //   "learner_id": number,
+// "assignment_id": number,
+// "submission": {
+//   "submitted_at": Date string,
+//   "score": number
+
+function getLearnerData(CourseInfo, AssignmentGroup, [LearnerSubmission]) {
+
+}
